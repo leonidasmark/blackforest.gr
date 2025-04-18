7a1004ae62efd9c2bacc5c3948de66eac6f39b72
John Doe <john.doe@contoso.com>
2025-04-18 21:39:12 +0300

    removed /el/

--- js/menu-footer-el.js
+++ js/menu-footer-el.js
@@ -1,11 +1,11 @@
-fetch("el/menu-el.html")
+fetch("menu-el.html")
 .then(response => response.text())
 .then(data => {
     document.getElementById("menu-el").innerHTML = data;
 })
 .catch(error => console.error("Error loading menu:", error));
 
-fetch("el/footer-el.html")
+fetch("footer-el.html")
 .then(response => response.text())
 .then(data => {
     document.getElementById("footer-el").innerHTML = data;
