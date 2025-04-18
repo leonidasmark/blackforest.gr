38987dce47ba0628524600e52d7b77ec31c3aa52
John Doe <john.doe@contoso.com>
2025-04-18 21:17:47 +0300

    language specific script loading

--- js/menu-and-footer.js
+++ js/menu-and-footer.js
@@ -1,27 +1,34 @@
-fetch("menu-en.html")
-.then(response => response.text())
-.then(data => {
-    document.getElementById("menu-en").innerHTML = data;
-})
-.catch(error => console.error("Error loading menu:", error));
+language = document.documentElement.lang
 
-fetch("menu-el.html")
-.then(response => response.text())
-.then(data => {
-    document.getElementById("menu-el").innerHTML = data;
-})
-.catch(error => console.error("Error loading menu:", error));
+function loadMenu(language) {
+    if (language="en"){
+        fetch("menu-en.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("menu-en").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading menu:", error));
 
-fetch("footer-en.html")
-.then(response => response.text())
-.then(data => {
-    document.getElementById("footer-en").innerHTML = data;
-})
-.catch(error => console.error("Error loading footer:", error));
+        fetch("footer-en.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer-en").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading footer:", error));
+    }
+    else if (language="el"){
+        fetch("menu-el.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("menu-el").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading menu:", error));
 
-fetch("footer-el.html")
-.then(response => response.text())
-.then(data => {
-    document.getElementById("footer-el").innerHTML = data;
-})
-.catch(error => console.error("Error loading footer:", error));
+        fetch("footer-el.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer-el").innerHTML = data;
+        })
+        .catch(error => console.error("Error loading footer:", error));
+    }
+}
